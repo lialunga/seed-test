d3f0767e1190f62c74f09e77d7ca2ff2572b41d6
plibapassil <passilpaulino@gmail.com>
2023-03-28 17:52:07 +0100

    feat: adiciona model de usuário

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid"
+
+
+@Entity("user")
+class User {
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    constructor(){
+        if(!this.id)
+            this.id = uuidv4();
+    }
+}
+
+export { User };
