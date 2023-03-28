538e1d29b8952979586f9b58288177ea70e7c216
plibapassil <passilpaulino@gmail.com>
2023-03-28 19:10:06 +0100

    feat: adiciona seed de usuário

--- src/dataSource/database.ts
+++ src/dataSource/database.ts
@@ -1,11 +1,14 @@
 import { DataSource, DataSourceOptions } from "typeorm"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const options: DataSourceOptions = {
     type: "postgres",
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
-    username: "postgres",
-    password: "0010",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     migrations: ["./src/dataSource/migrations/*.ts"],
     entities: ["./src/models/*.ts"]
---
+++ src/seeds/UserSeeder.ts
@@ -0,0 +1,19 @@
+import { hash } from "bcrypt";
+import { User } from "src/models/User";
+import { DataSource } from "typeorm";
+import { Seeder, SeederFactoryManager } from "typeorm-extension"
+
+export class UserSeeder implements Seeder {
+    async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any>{
+        const userRepository = dataSource.getRepository(User)
+
+        const userData = {
+            name: "Admin User",
+            email: "user@admin.com",
+            password: await hash("teste", 10)
+        }
+
+        const newUser = userRepository.create(userData)
+        await userRepository.save(newUser);
+    }
+}
